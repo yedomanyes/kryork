2d51bba028c37b4fddbccf8c3bf2f4d80c547ff2
ayscalekunden <onlinekurse2006@gmail.com>
2026-06-17 21:46:31 +0200

    fix: update supabase key to anon jwt

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://ivbezuedjwxgldlqouwi.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_auJgUDw2Wt8GCgoWwaZg9Q_Wj6voO3O';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2YmV6dWVkand4Z2xkbHFvdXdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MzE4NjksImV4cCI6MjA5NzIwNzg2OX0.CqB_qDU6kqRiT1I43ooiR3yqW3KpFFhM6vScGYjc49w';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
